f4ea125991d8657daa6dfa9e6bd72b580b847e76
Dan Crossley <dan.crossley@gmail.com>
2019-02-27 06:21:07 +0000

    First test
    List hosts

--- lraw.py
+++ lraw.py
@@ -7,8 +7,12 @@ class LRAW:
         self._token = token
         self._headers = {
         'Content-Type': "application/json",
-        'Authorization': "Bearer " + BEARER_TOKEN,
+        'Authorization': "Bearer " + token,
         'cache-control': "no-cache",
         }
     
-    
+    def list_hosts(self):
+        self.payload = ""
+        self.url = "https://" + self._server + ":8501/lr-admin-api/hosts/"
+        self.response = requests.request("GET", self.url, data=self.payload, headers=self._headers)
+        print(self.response.text)
